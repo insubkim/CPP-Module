c6d4679653e35f6089601ff303d63f21b715901e
root <j.j.kant1948@gmail.com>
2023-06-13 20:34:57 +0900

    feat : cpp-module01 ex02 done

---
+++ cpp-module-01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inskim <inskim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/20 05:12:43 by inskim            #+#    #+#             */
+/*   Updated: 2023/06/13 19:24:41 by inskim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringPTR = &s;
+    std::string &stringREF = s;
+    std::cout << &s << std::endl;
+    std::cout << &stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << s << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+}
