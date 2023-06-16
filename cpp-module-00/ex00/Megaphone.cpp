a4d1aab3bf479539cf23bbc2dfb35263aae232ef
Insub Kim <j.j.kant1948@gmail.com>
2023-06-17 01:06:37 +0900

    fixed : cpp-module00 ex00 megaphone class fixed with data perspective

--- cpp-module-00/ex00/Megaphone.cpp
+++ cpp-module-00/ex00/Megaphone.cpp
@@ -6,45 +6,51 @@
 /*   By: inskim <inskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 18:39:56 by inskim            #+#    #+#             */
-/*   Updated: 2023/05/13 03:06:23 by inskim           ###   ########.fr       */
+/*   Updated: 2023/06/17 01:05:31 by inskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
-class Megaphone
+class Upper
 {
-	private:
-		const static char *default_msg;
-	public:
-		void	turn_on(void);
-		void	turn_on(char	*msg);
+private:
+	const std::string	_upperString;
+	
+	static std::string convertToUpper(std::string original){
+		for (int i = 0; i < (int)original.size(); i++){
+			original[i] = toupper(original[i]);
+		}
+		return original;
+	}
+	
+public:
+	Upper(std::string originalString);
+	std::string getUpperString() const;
 };
 
-void	Megaphone::turn_on(void)
-{
-	std::cout << default_msg << std::endl;
+Upper::Upper(std::string originalString) : _upperString(convertToUpper(originalString)){}
+
+std::string Upper::getUpperString() const{
+	return _upperString;
 }
 
-void	Megaphone::turn_on(char	*msg)
-{
-	while (*msg)
-		std::cout << (char)toupper(*msg++);
+std::ostream& operator <<(std::ostream &o, const Upper& u){
+	std::cout << u.getUpperString();
+	return o;
 }
-
-const char	*Megaphone::default_msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 int	main(int argc, char *argv[])
 {
-	Megaphone m;
-	if (--argc == 0)
-		m.turn_on();
-	else
-	{
-		while (argc--)
-		{
-			argv++;
-			m.turn_on(*argv);
+	if (argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else{
+		for (int i = 1; i < argc; i++){
+			Upper u(argv[i]);
+			std::cout << u;
 		}
 		std::cout << std::endl;
 	}
