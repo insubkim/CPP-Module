b6e78a9c86a2e1a22a8eb3d658fcd0ddc496eeca
Insub Kim <j.j.kant1948@gmail.com>
2023-07-04 19:37:44 +0900

    feat : cpp-module-02 ex03 raycasting

--- cpp-module-02/ex03/main.cpp
+++ cpp-module-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inskim <inskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 05:12:43 by inskim            #+#    #+#             */
-/*   Updated: 2023/07/04 18:26:24 by inskim           ###   ########.fr       */
+/*   Updated: 2023/07/04 19:37:14 by inskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main( void ) {
     Point a(Fixed(0.0f), Fixed(0.0f));
     Point b(Fixed(0.0f), Fixed(6.0f));
     Point c(Fixed(3.0f), Fixed(3.0f));
-    Point p(Fixed(0.0f), Fixed(3.0f));
+    Point p(Fixed(3.0f), Fixed(2.0f));
     std::cout << bsp(a, b, c, p) << std::endl;
     return 0;
 }
